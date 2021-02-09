e62feb066a9a80a0b019d381742c34f3c2b894b6
Charlotte Davies <charlottemdavies@hotmail.co.uk>
2021-02-09 12:23:02 +0000

    ingredients list shows

--- IngredientsScreen.js
+++ IngredientsScreen.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import 'react-native-gesture-handler';
 
@@ -7,33 +7,43 @@ import * as recipeData from './recipes.json';
 
 export const IngredientsScreen = ({ navigation, route }) => {
 
+    const [ isLoading, setIsLoading ] = useState(true);
+    const [ allIngredients, setAllIngredients ] = useState();
+    const [ allRecipeData, setAllRecipeData ] = useState(recipeData);
+
     let allRecipeIngredients = [];
     let completeIngredientsList = [];
 
     function getIngredients() {
         Object.keys(recipeData).map(function(key) {
-            // console.log('1', recipeData)
             const recipeIngredients = recipeData[key].ingredients;
-            // console.log('2', recipeIngredients)
             if ( recipeIngredients !== undefined ) {
                 let allRecipeIngredients = addToIngredientsList(recipeIngredients);
             }
-            // console.log('4', allRecipeIngredients)
           })
-          const completeIngredientsList = allRecipeIngredients.reduce(function(a,b){if(a.indexOf(b)<0)a.push(b);return a;},[]);
-          console.log('5', completeIngredientsList);
+          let completeIngredientsList = allRecipeIngredients.reduce(function(a,b){if(a.indexOf(b)<0)a.push(b);return a;},[]);
+          setAllIngredients(completeIngredientsList);
           return completeIngredientsList;
-    }
+    };
 
     function addToIngredientsList(recipeIngredients) {
         recipeIngredients.map(ingredients => {
             allRecipeIngredients.push(ingredients)
         })
-        // console.log('3', allRecipeIngredients)
         return allRecipeIngredients;
-    }
+    };
 
-getIngredients()
+
+
+useEffect(() => {
+    getIngredients();
+}, [allRecipeData]);
+
+useEffect(() => {
+    if (allIngredients !== undefined) {
+        setIsLoading(false);
+    }
+}, [allIngredients]);
 
 
   return (
@@ -44,14 +54,14 @@ getIngredients()
               navigation.navigate('Recipes', { name: 'Jane' })
             }
         />
-        {completeIngredientsList !== undefined && (
-            completeIngredientsList.map(ingredients => {
-                <Text key={ingredients}>This {ingredients}</Text>
-            }
-        ))} 
-        {completeIngredientsList === undefined && (
-                <Text>undefined</Text>
-        )} 
+
+        {isLoading ? 
+            (<Text>undefined</Text>)
+            : 
+            (allIngredients.map(function(ingredients) {
+                return <Text >{ingredients}</Text>
+            }))
+        } 
     </View>
     );
 };
